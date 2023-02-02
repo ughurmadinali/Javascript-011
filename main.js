10abf0128c1bccb4721c5409e5e4183454bbdaed
ughurmadinali <120271577+ughurmadinali@users.noreply.github.com>
2023-02-02 19:53:23 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -5,22 +5,22 @@ console.log(a);
 
 2
 //задайте и выведите в консоли только год, месяц, день
-new Date('year', 'month', 'day')
+new Date('2023', '2', '2')
 console.log(Date);
 
 3
 //задайте и выведите в консоли только год, месяц, день, часы
-new Date('year', 'month', 'day', 'clock')
+new Date('2023', '2', '2', '19')
 console.log(Date);
 
 4
 //задайте и выведите в консоли только год, месяц, день, часы, минуты
-new Date('year', 'month', 'day', 'clock', 'menutes')
+new Date('2023', '2', '2', '19', '43')
 console.log(Date);
 
 5
 //задайте и выведите в консоли только год, месяц, день, часы, минуты, секунды
-new Date('year', 'month', 'day', 'clock', 'menutes', 'seconds')
+new Date('2023', '2', '2', '19', '43', '40')
 console.log(Date);
 
 6
@@ -34,23 +34,27 @@ let c = new Date("Jan 31 2023")
 
 8
 function kub(a) {
-    return 3 * 3 * 3
+    return a * a * a
 }
+console.log(kub(3))
 
 9
 
 
 10
 function power(a, b) {
-    return 230 * 10
+    return a * b
 }
+console.log(power(230, 10));
 
 11
-function seconds(c) {
-    return (60 * 60) * 2
+function seconds(c, d) {
+    return (c * c) * d
 }
+console.log(seconds(60, 2));
 
 12
-function triangleSlide(a, b) {
-    return (8 + 10) - 1
+function triangleSlide(a, b, e) {
+    return (a + b) e
 }
+console.log(trangleSlide(8, 10, -1));
