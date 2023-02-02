c410da6908cf0d8cb82ffc44f616829e1041930f
ughurmadinali <120271577+ughurmadinali@users.noreply.github.com>
2023-02-02 19:31:35 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,56 @@
+1
+//выведите в консоли полную дату
+const a = new Date()
+console.log(a);
+
+2
+//задайте и выведите в консоли только год, месяц, день
+new Date('year', 'month', 'day')
+console.log(Date);
+
+3
+//задайте и выведите в консоли только год, месяц, день, часы
+new Date('year', 'month', 'day', 'clock')
+console.log(Date);
+
+4
+//задайте и выведите в консоли только год, месяц, день, часы, минуты
+new Date('year', 'month', 'day', 'clock', 'menutes')
+console.log(Date);
+
+5
+//задайте и выведите в консоли только год, месяц, день, часы, минуты, секунды
+new Date('year', 'month', 'day', 'clock', 'menutes', 'seconds')
+console.log(Date);
+
+6
+//напиши краткие форматы дат
+let b = new Date("31/01/2023")
+
+7
+//напиши длинные форматы дат
+let c = new Date("Jan 31 2023")
+
+
+8
+function kub(a) {
+    return 3 * 3 * 3
+}
+
+9
+
+
+10
+function power(a, b) {
+    return 230 * 10
+}
+
+11
+function seconds(c) {
+    return (60 * 60) * 2
+}
+
+12
+function triangleSlide(a, b) {
+    return (8 + 10) - 1
+}
